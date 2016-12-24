65a57fdf34819ccf8e7a7f30dcec4af9de0c0ff4
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2016-12-24 15:55:14 -0800

    fix(macos): Re-enabled notification centre

--- macos/defaults/general.sh
+++ macos/defaults/general.sh
@@ -185,7 +185,7 @@ sudo systemsetup -setcomputersleep Off > /dev/null
 #
 # Disable Notification Center and remove the menu bar icon
 #
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+# launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 
 #
