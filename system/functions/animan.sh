4bea9b0a4ebf0bc5c5ba33ebe2685221f11ac5b0
Siyuan Gao <siyuangao@gmail.com>
2018-10-01 17:34:44 -0700

    Remove renamer dependency

--- system/functions/animan.sh
+++ system/functions/animan.sh
@@ -1,36 +1,54 @@
-#
+#!/bin/bash
 # Anime Management Toolbelt
 #
 # Author:  Denis Luchkin-Zhou <wyvernzora@gmail.com>
+#          Siyuan Gao <siyuangao@gmail.com>
 # License: MIT
 
-# TODO Install dependencies
-if [[ -n $(type foo >/dev/null 2>&1) ]]; then
-  npm install --global renamer
-fi
+# Set shellopt
+shopt -s nullglob
+shopt -s nocaseglob
 
 # Constants and expressions
-DELIMITER_L="\[\( 第"
-DELIMITER_R="\]\) 話"
-REGEX_INDEX="(\d{2}(?:\.\d+)?)(?:v\d+)?"
-REGEX_EXT="\.([\w\d]+)$"
+DELIMITER_L="\\[\\( 第"
+DELIMITER_R="\\]\\) 話"
+REGEX_INDEX="(\\d{2}(?:\\.\\d+)?)(?:v\\d+)?"
+REGEX_EXT="\\.([\\w\\d]+)$"
 
 # Rename anime in a folder
-aniren() {
-  BASENAME=$1
-  if [[ -z $BASENAME ]]; then
-    BASENAME=$(cwd);
-  fi
-  REGEX=".*(?:[$DELIMITER_L])$REGEX_INDEX(?:[$DELIMITER_R]).*$REGEX_EXT"
-  REPLACE=$BASENAME"[\$1].\$2"
-  renamer -e -f "$REGEX" -r "$REPLACE" --dry-run "*.{mp4,mkv,ass}"
+BASENAME=$1
+if [[ -z $BASENAME ]]; then
+  BASENAME=${PWD##*/};
+fi
+REGEX=".*(?:[$DELIMITER_L])$REGEX_INDEX(?:[$DELIMITER_R]).*$REGEX_EXT"
+REPLACE=$BASENAME" \$1.\$2"
 
-  echo
-  echo " - Press [Enter] to rename, [Ctrl-C] to cancel"
-  read -n 1
-
-  renamer -e -f "$REGEX" -r "$REPLACE" "*.{mp4,mkv,ass}" > /dev/null
-
-  echo
-  echo " - Complete!"
+function dry-run() {
+  printf "%s\t→\t%s\n" "$1" "$2"
 }
+
+function move() {
+  mv "$1" "$2"
+}
+
+function scan() {
+  for file in *.{mp4,mkv,ass}; do
+    NEWNAME=$(printf "%s\n" "$file" | perl -pe "s/$REGEX/$REPLACE/g")
+    ($1 "$file" "$NEWNAME")
+  done
+}
+
+scan dry-run
+
+echo
+echo " - Press [Enter] to rename, [Ctrl-C] to cancel"
+read -n 1
+
+scan move
+
+echo
+echo " - Complete!"
+
+# Unset shellopt
+shopt -u nullglob
+shopt -u nocaseglob
