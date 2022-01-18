080ec0a05ca88de9ad237ae852b28acef6874943
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2022-01-18 12:18:10 -0800

    Added basic fish config

---
+++ .config/fish/conf.d/colors.fish
@@ -0,0 +1,26 @@
+set -U fish_color_normal normal
+set -U fish_color_command 00d700
+set -U fish_color_quote af875f
+set -U fish_color_redirection d700af
+set -U fish_color_end 268bd2
+set -U fish_color_error dc322f
+set -U fish_color_param e4e4e4
+set -U fish_color_comment 586e75
+set -U fish_color_match normal
+set -U fish_color_selection c0c0c0
+set -U fish_color_search_match ffff00
+set -U fish_color_history_current normal
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd 008000
+set -U fish_color_cwd_root 800000
+set -U fish_color_valid_path normal
+set -U fish_color_autosuggestion 586e75
+set -U fish_color_user 00ff00
+set -U fish_color_host normal
+set -U fish_color_cancel normal
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+
